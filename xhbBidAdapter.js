495b329544b3848ca675e62f03c89491a7b331b9
maccordeiro <Marco.Cordeiro@groupm.com>
2017-12-20 17:33:04 +0000

    Change10

--- xhbBidAdapter.js
+++ xhbBidAdapter.js
@@ -2,8 +2,8 @@ import Adapter from './adapter.js';
 import bidfactory from './bidfactory.js';
 import bidmanager from './bidmanager.js';
 import * as utils from './utils.js';
-import { STATUS } from './constants.js';
-import adaptermanager from './adaptermanager.json';
+import { STATUS } from './constants.json';
+import adaptermanager from './adaptermanager.js';
 import { loadScript } from './adloader.js';
 
 const XhbAdapter = function XhbAdapter() {
